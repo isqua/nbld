7b1b6ab57a320391dc632479ef5623f3468b7bdd
isqua <isqua@isqua.ru>
2016-06-05 03:24:56 +0300

    Rename jade to pug

--- tasks/pug.js
+++ tasks/pug.js
@@ -1,16 +1,20 @@
-const jade = require('gulp-jade');
+const pug = require('gulp-pug');
+const data = require('gulp-data');
 const metrika = require('../lib/metrika');
 
 module.exports = (gulp, config) => {
 
-    gulp.task('jade:development', () => {
+    gulp.task('pug:development', () => {
         return gulp.src(config.src)
-            .pipe(jade())
+            .pipe(data(config.data || {}))
+            .pipe(pug())
             .pipe(gulp.dest(config.dest));
     });
 
-    gulp.task('jade:production', () => {
-        var pipe = gulp.src(config.src).pipe(jade());
+    gulp.task('pug:production', () => {
+        var pipe = gulp.src(config.src)
+            .pipe(data(config.data || {}))
+            .pipe(pug());
 
         if (config.metrika) {
             pipe = pipe.pipe(metrika(config.metrika));
